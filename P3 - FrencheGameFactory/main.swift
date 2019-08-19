332fa7969ff6c06534fa2176f99cd8c9a87c0694
roro <rodolphe.schnetzer@gmail.com>
2019-08-19 13:07:14 +0200

    Initial Commit

---
+++ P3 - FrencheGameFactory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  P3 - FrencheGameFactory
+//
+//  Created by Rodolphe Schnetzer on 19/08/2019.
+//  Copyright © 2019 Rodolphe Schnetzer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
